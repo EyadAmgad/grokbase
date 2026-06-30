97a40add4878c3880a1d31596e72338d022b69d4
Eyad Amgad <eyad.mahmoud@ejust.edu.eg>
2026-06-30 09:55:39 +0300

    add motor & create schemes structure

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -10,8 +10,11 @@ class Settings(BaseSettings):
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
 
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
+
     class Config:
         env_file = ".env"
 
 def get_settings():
-    return Settings()   
+    return Settings()
