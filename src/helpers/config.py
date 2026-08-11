7135af8c1f3a53826afb4d283bf1181d44933232
Eyad Amgad <eyad.mahmoud@ejust.edu.eg>
2026-08-12 02:20:54 +0300

    pgvector basic configs

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from typing import List
 class Settings(BaseSettings):
 
     APP_NAME: str
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
     GENERATION_DAFAULT_MAX_TOKENS: int = None
     GENERATION_DAFAULT_TEMPERATURE: float = None
 
-
+    VECTOR_DB_BACKEND_LITERAL: List[str] = None
     VECTOR_DB_BACKEND : str
     VECTOR_DB_PATH : str
     VECTOR_DB_DISTANCE_METHOD: str = None
